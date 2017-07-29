e19755a4ee9d6cfc402642c0397a9b1a9a79cfa1
Micheal Jouanneau <michaeljouanneau.dev@gmail.com>
2017-07-29 08:59:36 +0200

    Login Screen : Settting up main display

--- src/org/teamavion/app/Main.java
+++ src/org/teamavion/app/Main.java
@@ -10,12 +10,13 @@ import javafx.stage.StageStyle;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../assets/root.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setTitle("Team Avion Desktop Application");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage loginStage) throws Exception{
+        Parent LoginRoot = FXMLLoader.load(getClass().getResource("../assets/login.fxml"));
+        loginStage.initStyle(StageStyle.DECORATED);
+        loginStage.setTitle("Team Avion Desktop Application");
+        loginStage.setScene(new Scene(LoginRoot, 421.0, 581.0));
+        loginStage.setResizable(false);
+        loginStage.show();
     }
     public static void main(String[] args) {
         launch(args);
